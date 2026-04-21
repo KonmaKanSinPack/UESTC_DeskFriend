c38b4e5e21623cf5ea43a9487faf90d667669815
shiqi-svg <shiqi@yottalabs.ai>
2026-04-21 22:26:34 +0800

    更新生产-消费者结构用于处理消息队列
    脑壳痛

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import asyncio
 import sys
+import threading
 from xmlrpc import client
+
+import torch
+import numpy as np
 
 from PIL import ImageGrab
 import time
@@ -15,11 +19,16 @@ import qasync
 from openai import AsyncOpenAI
 import json
 import yaml
+import queue
+
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
 from PyQt5.QtCore import Qt, QPoint, QTimer 
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import QObject, pyqtSignal
-import torch
+
+import pyaudio
+from faster_whisper import WhisperModel
+
 
 def pil_image_to_base64(pil_image):
     buffered = BytesIO() # 制造一个存在于内存里的“虚拟文件”
@@ -59,7 +68,7 @@ class Vision: #AI的视觉模块
 
     async def look_at_screen(self):
         try:
-            screenshot = self.vision.sudden_view()
+            screenshot = self.sudden_view()
             screen_base64 = pil_image_to_base64(screenshot)
             img_msg = pack_msg("user", "image_url", f"data:image/png;base64,{screen_base64}")
 
@@ -76,22 +85,83 @@ class Listen(QObject):
         super().__init__()
         self.listen_history = deque(maxlen=history_length)
 
+        self.audio = pyaudio.PyAudio()
+
+        # 音频配置参数 (VAD 要求的标准格式)
+        self.SAMPLE_RATE = 16000 # 采样率：16kHz
+        self.CHUNK = 512         # 每次读取的音频块大小
+
+        # 开启麦克风数据流
+        self.stream = self.audio.open(
+            format=pyaudio.paInt16, # 16位深度
+            channels=1,             # 单声道
+            rate=self.SAMPLE_RATE,  # 16000 Hz
+            input=True,             # 这是一个输入流（麦克风）
+            frames_per_buffer=self.CHUNK # 每次读取 512 帧
+        )
+
         # 从 PyTorch Hub 自动加载 Silero VAD
         self.model, utils = torch.hub.load(
             repo_or_dir='snakers4/silero-vad',
             model='silero_vad',
             force_reload=False,
-            onnx=False
+            onnx=False,
+            trust_repo=True
         )
         self.get_speech_timestamps = utils[0] # 获取处理工具
-        
-        # 音频配置参数 (VAD 要求的标准格式)
-        self.SAMPLE_RATE = 16000 # 采样率：16kHz
-        self.CHUNK = 512         # 每次读取的音频块大小
 
-    async def get_voice_text(self):
-        self.text_signal.emit("胡桃我爱你！") #广播机制，并不需要传给谁，只要发出这个信号了，任何监听这个信号的对象都能收到并处理这个文本消息了
+        #加载whisper
+        self.whisper_model = WhisperModel("small", device="cuda", compute_type="float16")
+
+        self.start_threading()
+
+    def start_threading(self):
+        listen_thread = threading.Thread(target=self.during_listening, daemon=True) #daemon=True意思是：这个线程是个守护线程，主线程结束了它也会跟着结束，不会阻碍程序退出。
+        listen_thread.start()
+
+    def get_voice_text(self, text):
+        self.text_signal.emit(text) #广播机制，并不需要传给谁，只要发出这个信号了，任何监听这个信号的对象都能收到并处理这个文本消息了
     
+    def during_listening(self):
+        silence_timeout = 0
+        MAX_SILENCE = 20
+        while True:
+            #首先需要完成一个字节流-》numpy-》tensor的转换
+            voice_buffer = []
+            raw_bytes = self.stream.read(self.CHUNK)
+            audio_data = np.frombuffer(raw_bytes, dtype=np.int16).copy()
+            tensor_chunk = torch.from_numpy(audio_data).float() / 32768.0
+            #模型打分
+            score = self.model(tensor_chunk, self.SAMPLE_RATE).item()
+            if score >= 0.5:
+                print("检测到声音了，开始录音...")
+                voice_buffer.append(raw_bytes)
+                while silence_timeout < MAX_SILENCE:
+                    raw_bytes = self.stream.read(self.CHUNK)
+                    audio_data = np.frombuffer(raw_bytes, dtype=np.int16).copy()
+                    tensor_chunk = torch.from_numpy(audio_data).float() / 32768.0
+
+                    #模型打分
+                    score = self.model(tensor_chunk, self.SAMPLE_RATE).item()
+
+                    if score < 0.5:
+                        silence_timeout += 1
+
+                    else:
+                        silence_timeout = 0
+
+                    
+                    voice_buffer.append(raw_bytes)
+                
+                print("录音结束，正在转写...")
+                complete_audio_bytes = b"".join(voice_buffer)
+                complele_audio_np = np.frombuffer(complete_audio_bytes, dtype=np.int16).astype(np.float32) / 32768.0
+                # 把 complete_audio_bytes 交给 Whisper 转写
+                segments, info = self.whisper_model.transcribe(complele_audio_np, beam_size=5, language="zh")
+                transed_text = "".join([segment.text for segment in segments])
+                self.get_voice_text(transed_text)
+                silence_timeout = 0
+
 class Brain:
     def __init__(self): 
         with open("config.yaml", "r", encoding="utf-8") as f:
@@ -182,6 +252,13 @@ class Hutao(QWidget):
 
         # 连接听觉信号到处理函数
         self.listen.text_signal.connect(self.on_heard_text) #发射器.信号.connect(接收器)
+
+        #自锁
+        self.is_busy = False
+
+        #消息队列-》使用生产者-消费者结构实现
+        self.message_queue = asyncio.Queue()
+        asyncio.get_event_loop().create_task(self.on_received_message_consumer()) #创建一个后台任务，专门负责消费消息队列里的消息
         
     async def do_response(self, message):
         response = await self.brain.get_llm_response(message)
@@ -236,9 +313,29 @@ class Hutao(QWidget):
     @qasync.asyncSlot(str) #asyncSlot意思是：这个函数虽然是协程函数，但我要把它当成 Qt 的槽函数来用。普通的 PyQt 只认识同步槽函数，不会自动 await 你的协程。qasync 的这个装饰器会帮你把协程正确地挂到事件循环里执行。(str)意思是：这个槽函数期望接收一个 str 类型的信号参数。
     async def on_heard_text(self, text):
         print(f"接收到听觉消息：{text}")
-        should_reply = await self.should_reply(text)
-        if should_reply:
-            await self.do_response(text)
+        if not self.is_busy:
+            await self.message_queue.put(text) #把消息放到队列里，等着消费者去处理
+        else:
+            print("当前忙碌，暂时无法处理新的消息。")
+
+    # async def on_received_message_producer(self, message):
+    #     self.messsage_queue.put(message)
+
+    async def on_received_message_consumer(self):
+        while True:
+            message = await self.message_queue.get() #当队列为空时就会永远停留在这一行
+            self.is_busy = True
+            try:
+                print(f"正在处理消息：{message}")
+                if await self.should_reply(message):
+                    print("判断需要回复，正在处理消息...")
+                    await self.do_response(message)
+                    # print(response.choices[0].message.content)
+            except Exception as e:
+                print(f"处理消息时出错了：{e}")
+            finally:
+                self.is_busy = False
+                # self.message_queue.task_done() #和join成对出现，join表示开始，task_done表示结束。虽然现在这个队列没有调用join，但为了保持结构的完整性，还是调用一下task_done吧。
 
     def on_timer_trick(self):
         # resp = self.get_response("请你调用工具看看我的屏幕")
@@ -248,7 +345,7 @@ class Hutao(QWidget):
     def mouseDoubleClickEvent(self, event): #鼠标双击时
         if event.button() == Qt.LeftButton:
             print("接收到鼠标双击事件")
-            asyncio.create_task(self.do_response("用户刚刚通过鼠标触碰了你"))
+            self.message_queue.put_nowait("用户用鼠标触碰了你")
             event.accept()
     
     def mousePressEvent(self, event):
@@ -267,7 +364,6 @@ if __name__ == "__main__":
 
     loop = qasync.QEventLoop(app) #创建兼容PyQt的异步事件
     asyncio.set_event_loop(loop) #设置异步事件循环
-
 
     hutao = Hutao()
     hutao.show()
